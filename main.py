9cd9d63e3e143e2a4db98a12f22136900a807a56
Vlad Danilchuk <vladdanilchuk9@gmail.com>
2024-02-08 08:05:17 +0200

    rename i -> y and j -> x

--- main.py
+++ main.py
@@ -17,24 +17,27 @@ run = True
 def draw_board(lvl):
     num1 = ((height - 50) // 32)
     num2 = (width // 30)
-    for i in range(len(lvl)): # висота num1
-        for j in range(len(lvl[i])):# ширина num2
-            if level[i][j] == 1:
-                pygame.draw.circle(screen, "white", (j * num2 + 0.5 * num2, i * num1 + 0.5* num1), 2)
-            if level[i][j] == 2:
-                pygame.draw.circle(screen, "white", (j * num2 + 0.5 * num2, i * num1 + 0.5* num1), 6)
-            if level[i][j] == 3:
-                pygame.draw.line(screen, color, (j * num2 + (0.5 * num2),i * num1),
-                                                 (j * num2 + (0.5 * num2),i * num1+num1), 2)
-            if level[i][j] == 4:
-                pygame.draw.line(screen, color, (j * num2, i * num1 + (0.5 * num1)),
-                                 (j * num2 + num2, i * num1 + (0.5 * num1)), 2)
+    for y in range(len(lvl)): # висота num1
+        for x in range(len(lvl[y])):# ширина num2
+            if level[y][x] == 1:
+                pygame.draw.circle(screen, "white", (x * num2 + 0.5 * num2, y * num1 + 0.5* num1), 2)
+            if level[y][x] == 2:
+                pygame.draw.circle(screen, "white", (x * num2 + 0.5 * num2, y * num1 + 0.5* num1), 6)
+            if level[y][x] == 3:
+                pygame.draw.line(screen, color, (x * num2 + (0.5 * num2),y * num1),
+                                                 (x * num2 + (0.5 * num2),y * num1+num1), 2)
+            if level[y][x] == 4:
+                pygame.draw.line(screen, color, (x * num2, y * num1 + (0.5 * num1)),
+                                 (x * num2 + num2, y * num1 + (0.5 * num1)), 2)
+            if level[y][x] == 9:
+                pygame.draw.line(screen, "white", (x * num2, y * num1 + (0.5 * num1)),
+                                 (x * num2 + num2, y * num1 + (0.5 * num1)), 3)
 
 
 
 while run:
     timer.tick(fps)
-    screen.fill("Red")
+    screen.fill("black")
     draw_board(level)
 
     for event in pygame.event.get():
