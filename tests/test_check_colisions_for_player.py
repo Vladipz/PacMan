465ebf017cb2ebbb5ab215eac1c46f3527dacbb6
DmytroHoncharuk <dima2005oleksandr@gmail.com>
2024-04-18 15:00:33 +0300

    added test for collisions

---
+++ tests/test_check_colisions_for_player.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+# 0-RIGHT, 1-LEFT, 2-UP, 3-DOWN
+@pytest.mark.parametrize("center_x, center_y, direction, expected_turns", [
+    (73, 74, 3, [False, False, True, False]),
+    (39, 40, 1, [True, False, False, False]),
+    (113, 104, 2, [False, False, False, False])
+])
+def test_check_position(maze_with_huge_map, center_x, center_y, direction, expected_turns):
+    maze_with_huge_map.player.direction = direction
+    turns = maze_with_huge_map.check_position(center_x, center_y)
+    assert turns == expected_turns
