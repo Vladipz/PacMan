5bdb73c5071d09830dcd35d39acd5bb28daf70b6
DmytroHoncharuk <dima2005oleksandr@gmail.com>
2024-04-18 11:14:40 +0300

    added test for player movement, added fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,32 @@
+import pytest
+import pygame
+from maze import Maze
+from Player import Player
+
+@pytest.fixture
+def maze_with_player():
+    color = (255, 255, 255)
+    width = 800
+    height = 600
+    screen = None
+    maze_grid = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+    maze_obj = Maze(color, width, height, screen)
+    maze_obj.level = maze_grid
+
+    player = Player(4, 4, 3, 0, 2, maze_obj)
+    maze_obj.player = player
+    return maze_obj
 
 
 
