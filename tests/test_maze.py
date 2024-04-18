4bf8297fe4c201f8a25b099db18e0e0b616f207a
Vlad Danilchuk <vladdanilchuk9@gmail.com>
2024-04-18 12:25:33 +0300

    add test for check win method maze class

--- tests/test_maze.py
+++ tests/test_maze.py
@@ -4,6 +4,12 @@ from settings import *
 
 
 @pytest.fixture
+def mock_maze(mocker):
+    # Mock maze object
+    return mocker.MagicMock()
+
+
+@pytest.fixture
 def maze_instance():
     maze = Maze("white", width, height, screen=None)
     return maze
@@ -21,9 +27,23 @@ def test_find_coordinates_for_health(maze_instance):
     # assert maze_instance.find_coordinates_for_health() == (2, 2)
     # Modify the level to have some points
     # Modify the level according to your game structure
-    maze_instance.level = [
-        [0, 0, 0],
-        [1, 0, 1],
-        [0, 0, 0]]
+    maze_instance.level = [[0, 0, 0], [1, 0, 1], [0, 0, 0]]
     # random.seed(42)  # Set seed for reproducibility
     assert maze_instance.find_coordinates_for_health() == (1, 0) or (1, 2)
+
+
+# def mock_check_point_on_board_1():
+#     return 0  # For example, always return 0 for testing purposes
+#
+#
+# def mock_check_point_on_board_2():
+#     return 5  # For example, always return 5 for testing purposes
+
+
+@pytest.mark.parametrize("mocked_method", [lambda: 0, lambda: 5])
+def test_check_win_with_mocked_method(maze_instance, monkeypatch, mocked_method):
+    # Use monkeypatch to substitute the method
+    monkeypatch.setattr(maze_instance, "check_point_on_board", mocked_method)
+
+    # Now, check_win should return True or False based on the mocked_method
+    assert maze_instance.check_win() == (True if mocked_method() == 0 else False)
